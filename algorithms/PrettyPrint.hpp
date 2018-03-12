b5e6ce490cb9c9cd2cc058544c791295c5e284c7
Carl Morris <carl.morris.world@gmail.com>
2018-03-11 23:27:10 -0700

    Started refactoring usage printer.

--- algorithms/PrettyPrint.hpp
+++ algorithms/PrettyPrint.hpp
@@ -3,23 +3,28 @@
 #include <iostream>
 #include <unordered_map>
 
-using std::ostream;
-using std::unordered_map;
-
 template<class keyT, class valueT>
-ostream& operator<<(ostream& os, const unordered_map<keyT, valueT>& out_map) {
+std::ostream& operator<<(std::ostream& os, const std::unordered_map<keyT, valueT>& out_map) {
 	for (auto it : out_map) {
-		cout << "(" << it.first << ", " << it.second << ")\n";
+		os << "(" << it.first << ", " << it.second << ")\n";
 	}
 
 	return os;
 }
 
 template<class keyT, class valueT>
-ostream& operator<<(ostream& os, const unordered_map<keyT, valueT*>& out_map) {
+std::ostream& operator<<(std::ostream& os, const std::unordered_map<keyT, valueT*>& out_map) {
 	for (auto it : out_map) {
-		cout << "(" << it.first << ", " << *(it.second) << ")\n";
+		os << "(" << it.first << ", " << *(it.second) << ")\n";
 	}
 
+	return os;
+}
+
+template<class valueT>
+std::ostream& operator<<(std::ostream& os, const std::vector<valueT> vec) {
+	for (auto& it = begin(vec); it != end(vec); ++it) {
+		os << *it << "\n";
+	}
 	return os;
 }
--- algorithms/Usage.cpp
+++ algorithms/Usage.cpp
@@ -1,52 +1,135 @@
+#include "Logging.hpp"
+#include "PrettyPrint.hpp"
+
 #include <string>
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
 
 // <filesystem> was not in the standard at the time of relase for Visual Studio 2017 .
 #include <experimental/filesystem> // C++-standard header file name  
 #include <filesystem> // Microsoft-specific implementation header file name  
 
 namespace fs = std::experimental::filesystem::v1;
-using std::string;
-using std::cout;
-using std::endl;
+using namespace std;
+
+class  ColumnarText {
+public:
+	ColumnarText(const vector<vector<string>>& columnEntries, const string&, int inIdent);
+
+	void print(ostream&) const;
+
+private:
+	int indent;
+	string columnSeparator;
+	vector<vector<string>> textMatrix;
+	vector<unsigned int> columnWidths;
+};
+
+ColumnarText::ColumnarText(
+	const vector<vector<string>>& columnEntries, 
+	const string& inColumnSeparator="  ", 
+	int inIdent=2) :
+		indent{inIdent},
+		columnSeparator{inColumnSeparator}
+	{
+	//TODO: Handle empty inputs.
+
+	//Require same number of columns each row.
+	auto nColumns = columnEntries[0].size();
+	columnWidths = vector<unsigned int>(nColumns, 0);
+
+	for (const auto& row : columnEntries) {
+		LOG_DEBUG(
+			cout << "DEBUG row of columnEntries\n" << row;
+		);
+		auto columnWidth = columnWidths.begin();
+		auto entries = vector<string>(nColumns);
+		auto entriesIt = entries.begin();
+		for (const auto& entry : row) {
+			//TODO: Strip extra white space and handle new lines gracefully.
+			*columnWidth = max(*columnWidth, static_cast<unsigned int>(entry.length()));
+			*entriesIt = entry;
+			++columnWidth;
+			++entriesIt;
+		}
+		textMatrix.push_back(entries);
+	}
+}
+
+
+void ColumnarText::print(ostream& os) const {
+	auto initialIndent = string(indent, ' ');
+	for (const auto& row : textMatrix) {
+		os << initialIndent;
+		auto columnWidth = columnWidths.begin();
+		for (auto& entry = begin(row); entry != end(row); ++entry) {
+			os << setw(*columnWidth) << left
+				<< *entry
+				<< ((entry == end(row) - 1) ? "\n" : columnSeparator);
+			++columnWidth;
+		}
+	}
+}
+
+
+ostream& operator<<(ostream& os, const ColumnarText& text) {
+	text.print(os);
+	return os;
+}
+
 
 void printUsage(int argc, char** argv) {
 	string programNameArg(argv[0]);
 	fs::path programPath(programNameArg);
-	string programFilename(programPath.filename().string());
+	auto programFilename = programPath.filename().string();
+
+	auto pdfCdfParameterText = ColumnarText({
+			{ "rate", "Rate of arrivals in Poisson process." },
+			{ "duration", "Duration of interval over while to calculate probability." },
+			{ "number", "Number of arrivals to calculate probability" }
+		});
+	auto sampleArrivalTimesParameterText = ColumnarText({
+			{ "rate", "Rate of arrivals in Poisson process."},
+			{ "number", "Number of arrival times to sample."},
+			{ "seed", "Seed for random number generator."}
+		});
+	auto sampleNumberArrivalsParameterText = ColumnarText({
+			{"rate", "Rate of arrivals in Poisson process."},
+			{"duration", "Duration of interval in which to count arrivals."},
+			{"seed", "Seed for random number generator."}
+		});
+	auto matrixTestParameterTextMatrix = vector<vector<string>>();
+	matrixTestParameterTextMatrix.push_back(vector<string>{ "test-matrix", "Filename of matrix to use for tests." });
+	auto matrixTestParameterText = ColumnarText(matrixTestParameterTextMatrix);
 
 	cout
 		<< "Usage 1: " << programFilename << " poisson-process {pmf|cdf|sample-arrival-times} args\n\n"
 		<< "pmf|cdf\n"
 		<< "Choose whether to evaluate pmf or cdf.\n"
-		<< "  rate      Rate of arrivals in Poisson process.\n"
-		<< "  duration  Duration of interval over which to calculate probability.\n"
-		<< "  number    Number of arrivals to calculate probability\n"
+		<< pdfCdfParameterText
 		///////////////////////////////////////////////////////////////////////////////
-		<< "\n"
-		"Calculates the probability P(N(t+s) - N(t) {=|<=} k) = exp(-L*s) * (L*s)**k / k!\n"
+		<< endl
+		<< "Calculates the probability P(N(t+s) - N(t) {=|<=} k) = exp(-L*s) * (L*s)**k / k!\n"
 		"for a Poisson process N with rate of arrivals L equal to the argument \"rate\",\n"
 		"over an interval of length s equal to the argument \"duration\", for a number\n"
 		"of arrivals k equal to the argument \"number\". The sign \"=\" or \"<=\" is chosen\n"
 		"by selecting pmf or cdf respectively.\n"
-		<< "\n"
+		<< endl
 		<< "sample-arrival-times\n"
 		<< "Choose to sample a sequence of arrival time variates.\n"
-		<< "  rate    Rate of arrivals in Poisson process.\n"
-		<< "  number  Number of arrival times to sample.\n"
-		<< "  seed    Seed for random number generator.\n"
-		<< "\n"
+		<< sampleArrivalTimesParameterText
+		<< endl
 		<< "sample-number-arrivals\n"
 		<< "Choose to sample the number of arrivals in and interval.\n"
-		<< "  rate      Rate of arrivals in Poisson process.\n"
-		<< "  duration  Duration of interval in which to count arrivals.\n"
-		<< "  seed      Seed for random number generator.\n"
-		<< "\n"
+		<< sampleNumberArrivalsParameterText
+		<< endl
 		////////////////////////////////////////////////////////////////////////////////
 		<< "Usage 2: " << programFilename << " matrix test args\n\n"
 		<< "test\n"
 		<< "Choose whether to test matrix functions.\n"
-		<< "  test-matrix  Filename of matrix to use for tests.\n"
+		<< matrixTestParameterText
 		///////////////////////////////////////////////////////////////////////////////
 		<< "\n"
 		"Calculates various matrix operations.\n"
